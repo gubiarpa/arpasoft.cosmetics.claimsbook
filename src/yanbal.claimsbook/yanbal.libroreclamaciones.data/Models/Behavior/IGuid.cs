31f517f78870f27ebe39c8f344d1afd4e5702b90
gubiarpa <billy.arredondo@pucp.pe>
2020-09-02 16:35:57 -0500

    dependencies project created
    project data, project repository created

---
+++ src/yanbal.claimsbook/yanbal.libroreclamaciones.data/Models/Behavior/IGuid.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace yanbal.libroreclamaciones.data.Models.Behavior
+{
+    public interface IGuid
+    {
+        Guid ID { get; set; }
+    }
+}
---
+++ src/yanbal.claimsbook/yanbal.libroreclamaciones.repository/DBContextApp.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace yanbal.libroreclamaciones.repository
+{
+    public class DBContextApp : DbContext
+    {
+        public DBContextApp(DbContextOptions<DBContextApp> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
