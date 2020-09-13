43c20e200aa0cfef611ad7955b78f207f727db1d
gubiarpa <billy.arredondo@pucp.pe>
2020-09-13 07:37:03 -0500

    filling dropdown zone-subzone

--- src/yanbal.claimsbook/yanbal.claimsbook.web/wwwroot/js/site.js
+++ src/yanbal.claimsbook/yanbal.claimsbook.web/wwwroot/js/site.js
@@ -4,5 +4,9 @@
  * displayValue : Valor visualizado por el usuario
  */
 function printOptionInSelect(dataValue, displayValue) {
-    return `<option ${ dataValue == null ? 'disabled selected' : ''} value='${dataValue}'>${displayValue}</option>`;
+    return `<option ${ dataValue == null ? 'disabled selected' : ''} value=${quoteNull(dataValue)}>${displayValue}</option>`;
+}
+
+function quoteNull(value) {
+    return value == null ? null : `'${value}'`;
 }
