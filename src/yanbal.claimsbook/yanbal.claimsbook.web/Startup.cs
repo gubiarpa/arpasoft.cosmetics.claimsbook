da46b19e281d173692b3318aabd02cd7f16ab791
Luis Rojas <luis.rojas@yanbal.com>
2020-10-06 11:46:27 -0500

    se agregó configuración de application insights

--- src/yanbal.claimsbook/yanbal.claimsbook.web/Startup.cs
+++ src/yanbal.claimsbook/yanbal.claimsbook.web/Startup.cs
@@ -25,6 +25,8 @@ namespace yanbal.claimsbook.web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
+
             services.AddCors(options =>
             {
                 options.AddDefaultPolicy(
