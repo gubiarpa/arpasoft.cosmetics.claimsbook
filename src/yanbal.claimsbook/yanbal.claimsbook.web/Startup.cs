358c2dd9f706d9959936b0075f67c89dc4c64c88
gubiarpa <billy.arredondo@pucp.pe>
2020-10-12 18:14:54 -0500

    prod step

--- src/yanbal.claimsbook/yanbal.claimsbook.web/Startup.cs
+++ src/yanbal.claimsbook/yanbal.claimsbook.web/Startup.cs
@@ -37,7 +37,7 @@ namespace yanbal.claimsbook.web
             });
             services.AddControllersWithViews();
             services.AddDbContext<DBContextApp>(
-                options => options.UseSqlServer(Configuration.GetConnectionString("YanbalClaimsConn")));
+                options => options.UseSqlServer(Configuration.GetConnectionString("ClaimsBookConn")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
